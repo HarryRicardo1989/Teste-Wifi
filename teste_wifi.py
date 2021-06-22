6748c5e3d4d1df66169211e0a23662a40787f375
Ricardo Paschoali <ricardo.paschoali@gmail.com>
2021-06-22 08:03:22 -0300

    Divisao de Responsabilidades

--- teste_wifi.py
+++ teste_wifi.py
@@ -1,27 +1,11 @@
 #!/usr/bin/python3
 from flask import Flask
+from RequestCommands import RequestCommands
 import os
 from time import sleep
 
 app = Flask(__name__)
-
-
-class RequestCommands:
-    def __init__(self):
-        pass
-
-    def comand(self, comando):
-        retorno = os.popen(comando)
-        return retorno.read().strip().strip('\n').strip()
-
-    def grava_resultado(self,nome, angulo,metros, signal_power1, signal_power2,signal_power3,signal_power4,signal_power5):
-        with open(f'./teste-Wifi-{nome}.csv', 'a') as file:
-            file.writelines(f'angulo:{angulo}|{metros}m|{signal_power1}|{signal_power2}|{signal_power3}|{signal_power4}|{signal_power5} \n')
-        return self.le_arquivo(nome)
-    def le_arquivo(self,nome):
-        with open(f'./teste-Wifi-{nome}.csv', 'r') as arquivo:
-            return arquivo.read()
-            
+         
 comandos = RequestCommands()
     
            
